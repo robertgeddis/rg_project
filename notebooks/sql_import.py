4df6e56f453ee9b7dfd71934bc122f6ec8132a45
robertgeddis <robert.geddis@hotmail.co.uk>
2026-02-16 17:59:38 +0100

    Save Haven hypothesis results

--- notebooks/sql_import.py
+++ notebooks/sql_import.py
@@ -1,26 +1,8 @@
-from nb_setup_v2 import get_assets, list_tables
+# SQL Engine
+import sqlalchemy as sa
 
-pd, engine = get_assets()
-print(f"Success! Tables: {list_tables()}")
+db_path = r"C:\Users\rober\Documents\Python\rg_project\data\rg_project_database.db"
+engine = sa.create_engine(f"sqlite:///{db_path}")
 
-# Enter the code above as the first cell in .ipynb files
-# Use syntax below afterwards to pull data
-
-
-''' 
-query = """
-SELECT 
-    m.Date, 
-    m.M2, 
-    f.SP500, 
-    f.BTC, 
-    f.Gold
-FROM macro_data m
-LEFT JOIN market_data_filled f ON m.Date = f.Date
-WHERE f.SP500 IS NOT NULL
-ORDER BY m.Date ASC
-"""
-
-df = pd.read_sql(query, engine)
-df['Date'] = pd.to_datetime(df['Date'])
-df.head() '''
+inspector = sa.inspect(engine)
+print(f"Tables found: {inspector.get_table_names()}")
--- sql_engine.py
+++
@@ -1,4 +0,0 @@
-from db_config import get_engine
-
-engine = get_engine()
-# df = pd.read_sql('macro_data', engine)
